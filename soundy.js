8ead4de150dc02fced76e91edb43de808a611c45
Ivaniku <56933254+Ivaniku@users.noreply.github.com>
2021-01-23 13:00:38 +0100

    Added comments

--- soundy.js
+++ soundy.js
@@ -1,13 +1,18 @@
+//This part of the code initializes all variables from the Modules folder. Remember to change it whenever you install a dependency. 
 const { Discord, Client, token, prefix, fs, os, HelpMenu, InviteMenu, tts, util, ActivitiesList } = require("./Modules/Variables.js");
 const { SaveTTS } = require("./Modules/Functions.js");
 
+//The variable isReady determines whenever the bot is able to play an audio or not. Be careful, it can cause problems sometimes.
 isReady = true;
 
+//This part of the code is executed when the bot logs in, also used for random status.
 Client.on('ready', () => {
     console.log("Logged in!");
+    //This whole piece of code is the random status.
     var SoundyHelp = true
     setInterval(() => {
         if (SoundyHelp == true){
+            //This is the default status that executes between the random statuses.
             Client.user.setActivity("Type \"soundy help\"");
             SoundyHelp = false
         }else{
@@ -16,11 +21,15 @@ Client.on('ready', () => {
             SoundyHelp = true
         }
     }, 10000)
+    //^^ You can change that number to change the interval of time between every status in miliseconds.
 });
 
+//This part of the code is executed when a message is sent.
 Client.on('message', message => {
     const args = message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift()
+    //This is the Play function, i couldn't declare it in the Modules folder because it wouldn't recognise the variables.
+    //I'm pretty sure that declaring it right here is a bad way of doing it and makes the code look messier but it's the only workaround i could find
     function Play(Wtp) {
         try{
             voiceChannel = message.member.voice.channel;
@@ -51,8 +60,10 @@ Client.on('message', message => {
             Client.users.cache.get("561560432100245520").send("An error ocurred playing an audio btw, lemme give you more info: \n```\n" + err + "\n```");
         }
     }
+    //This piece of code is executed when the message that is sent doesn't contain the Soundy prefix. Do not touch.
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     else if (isReady || command.toLowerCase() == "stop" || command.toLowerCase() == "eval" || command.toLowerCase() == "eval") {
+        //This is the switch statement that contains all the commands, i'm pretty happy i used a switch. It makes the code look a lot prettier and more efficient. Adding a command is also very easy.
         switch(command.toLowerCase()){
             case "stop":
                 if (message.guild.me.voice.channel != null){
@@ -73,6 +84,7 @@ Client.on('message', message => {
                 }
                 break
             case "ping":
+                //I'm sure this isn't a very efficient way of converting a negative number to a positive one but i'll find another workaround.
                 let Ping = message.createdTimestamp - Date.now()
                 message.channel.send("The ping is \"" + `${Ping.toString().slice(1)}` + " ms\"");
                 break
@@ -80,11 +92,13 @@ Client.on('message', message => {
                 message.channel.send(InviteMenu)
                 break
             case "info":
+                //TODO: Give this thing a use. It's kinda useless and looks ugly.
                 os.cpuUsage(function(v){
                     message.channel.send("Take some info about my life!\n\n**System Info**:\n```\nCpu Usage: " + Math.round(v) + "%\nUsed Memory: " + Math.round(os.freememPercentage()) + "%\n```")
                 });
                 break
             case "eval":
+                //The eval command, not the best way of doing eval but it works like a charm ^_^
                 if(message.author.id !== "561560432100245520"){
                     message.channel.send("Woah there you know Eval is dangerous af");
                     return;
@@ -102,12 +116,14 @@ Client.on('message', message => {
                 }
                 break
             case "tts":
+                //The TTS, the API i'm using is kinda unstable so it sometimes throws unwanted results. But i'm still pretty hapy with it.
                 if (args.length > 0){
                         SaveTTS(Play("TTS.mp3"), message.content.split(' ').splice(2).join(' '))
                 }else
                 message.channel.send("It would be good if you uuuh... Provided text")
                 break
             case "say":
+                //I'm planning to make this command optional in the config menu i have planned since it can rate limit Soundy and increase raids.
                 if (args.length > 0){
                     message.delete()
                     message.channel.send("Sending message...").then((sentMessage) => sentMessage.edit(message.content.split(' ').splice(2).join(' ')))
@@ -115,9 +131,11 @@ Client.on('message', message => {
                 message.channel.send("*says nothing*")
                 break
             case "snipe":
+                //Hahaha inspiration on dankmemer go brrrrr
                 message.channel.send("Here you have", {files: ["./TTS.mp3"]})
                 break
             default:
+                //This code executes when none of the commands are executed and searchs for a sound. This is a very efficient way fo doing it.
                 try {
                     if (fs.existsSync('./Audio/' + command.toLowerCase() + ".mp3")) {
                       message.delete();
@@ -131,9 +149,15 @@ Client.on('message', message => {
                 break
         }
     }
+
+    //This piece of code executes when the bot isn't ready to play sounds,
+    //though for some reason i don't want to know it also wxecutes when the Discord API rate limits Soundy and breaks the bot.
+    //So I also made an error handler inside.
     else if (isReady == false)
     {
         if (isReady == true){
+            //This is when the bot detects that it's broken and DM's me,
+            //it tries to make some fixes like declaring the variables again and setting isReady to true
             message.channel.send("Hold on! An unknown error occured")
             Client.users.cache.get("561560432100245520").send("An error occured, check it lazy dumbass https://cp.something.host/services/1509%22%22 ");
             const { Discord, Client, token, prefix, fs, os, HelpMenu, InviteMenu, tts, util, ActivitiesList } = require("./Modules/Variables.js");
